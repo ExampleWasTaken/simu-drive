df6f4917428f2ebe986b63e67a47942c3d8a1760
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-09-23 20:22:07 +0200

    docs: fix dangling javadoc comment

--- src/main/java/nz/ewt/core/hardware/HardwareComponent.java
+++ src/main/java/nz/ewt/core/hardware/HardwareComponent.java
@@ -4,7 +4,6 @@ package nz.ewt.core.hardware;
  * Represents a hardware component meaning it is managed by the {@link nz.ewt.core.hardware.HardwareOrchestrator} and runs
  * at simulation speed.
  */
-
 import nz.ewt.core.common.Component;
 
 public interface HardwareComponent extends Component {
