cdb2889443160da93ac5f2be7945da3ee2423944
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-09-25 00:51:16 +0200

    style: remove unnecessary thrown declaration

--- src/main/java/nz/ewt/Main.java
+++ src/main/java/nz/ewt/Main.java
@@ -4,7 +4,7 @@ import nz.ewt.core.SimulationEngine;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         System.out.println("RUNNING...");
 
         SimulationEngine engine = new SimulationEngine();
